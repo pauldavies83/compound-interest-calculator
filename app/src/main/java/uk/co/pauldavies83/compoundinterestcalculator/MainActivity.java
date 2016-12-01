eb722acb60665e695f7b826df0a566033396cc6a
pauld03 <paul.davies3@bbc.co.uk>
2016-12-01 12:37:46 +0000

    Initial commit

---
+++ app/src/main/java/uk/co/pauldavies83/compoundinterestcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.co.pauldavies83.compoundinterestcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
